a59eb42c41e54532e0d7e4d167631ac1df69a028
rafaRM002 <156197209+rafaRM002@users.noreply.github.com>
2025-05-12 14:02:00 +0200

    Cambio de apiComunicator
    Modificacion para comprobar la recarga automatica tras cambios

--- proyecto-fontend/src/utilities/apiComunicator.jsx
+++ proyecto-fontend/src/utilities/apiComunicator.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const useApi = (endpoint, options = {}) => {
@@ -10,43 +10,42 @@ const useApi = (endpoint, options = {}) => {
   const [error, setError] = useState(null);
   const [currentOptions, setCurrentOptions] = useState(options);
 
-  // Función fetchData fuera del useEffect
-  const fetchData = useCallback(async () => {
-    if (!uri || !currentOptions) return;
-
-    try {
-      setLoading(true);
-      const response = await axios({
-        url: uri,
-        method: currentOptions.method || 'GET',
-        headers: {
-          ...(currentOptions.headers || {}),
-        },
-        withCredentials: true,
-        data: currentOptions.body || undefined,
-      });
-
-      setData(response.data);
-      setError(null);
-    } catch (err) {
-      console.error("Error during request:", err);
-      if (err.response) {
-        setError(`Error ${err.response.status}: ${err.response.data}`);
-      } else if (err.request) {
-        setError("No response received from the server");
-      } else {
-        setError(err.message);
-      }
-      setData(null);
-    } finally {
-      setLoading(false);
-    }
-  }, [uri, currentOptions]);
-
-  // Ejecutar cuando cambien uri u options
   useEffect(() => {
+    const fetchData = async () => {
+      if (!uri || !currentOptions) return;
+
+      try {
+        setLoading(true);
+        const response = await axios({
+          url: uri,
+          method: currentOptions.method || 'GET',
+          headers: {
+            ...(currentOptions.headers || {}),
+          },
+          
+          withCredentials: true,
+          data: currentOptions.body || undefined,
+        });
+
+        setData(response.data);
+        setError(null);
+      } catch (err) {
+        console.error("Error during request:", err);
+        if (err.response) {
+          setError(`Error ${err.response.status}: ${err.response.data}`);
+        } else if (err.request) {
+          setError("No response received from the server");
+        } else {
+          setError(err.message);
+        }
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
     fetchData();
-  }, [fetchData]);
+  }, [uri, currentOptions]); // Ejecutar cuando cambien uri u options
 
   return {
     data,
@@ -56,7 +55,6 @@ const useApi = (endpoint, options = {}) => {
     setError,
     setOptions: setCurrentOptions,
     currentOptions,
-    refetch: fetchData, // Exponemos la función
   };
 };
 
